e0944e908f40b121afa196922221e8acd56276f4
Sara <saramf240614@gmail.com>
2025-03-25 21:53:30 -0300

    Agregado error de página no encontrada y creada página de productos (aun sin nada)

--- app.js
+++ app.js
@@ -5,9 +5,27 @@ const app = express()
 //middlewares
 app.use(express.json())
 
-//main route
+
 app.get('/', (req, res, next) => {
     res.send('Bienvenido a Nodepop')
+})
+
+
+app.get('/products', (req, res, next) => {
+    res.send('Aquí van los productos')
+})
+
+// error object with error code
+app.use((req, rest , next) => {
+    const error = new Error('página no encontrada')
+    error.status = 400 //user error
+    next(error)
+})
+
+//Middleware error handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500) //if we don't know where the error is coming from we assume it's ours
+    res.send('Ocurrió un error: ' + err.message)
 })
 
 export default app
--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const port = process.env.PORT || 3000
 //create http server
 const server = http.createServer(app)
 
+
+//when emitting an error type event
 server.on('error', err => console.error(err))
+
 server.on('listening', () => {
     console.log(`Server started on http://localhost:${port}`)
 })
