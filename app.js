e4815d3f738875f02817fffd3f7460407a505ec8
Sara <saramf240614@gmail.com>
2025-06-21 13:14:33 -0400

    Internacionalización aplicada: traducimos de inglés a español todas las páginas

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import connectMongoose from './lib/connectMongoose.js'
 import ejs from 'ejs'
 import * as sessionManager from './lib/sessionManager.js'
 import { logoutUser } from './controllers/loginController.js'
+import i18n from './lib/i18nConfigure.js'
 
 
 await connectMongoose()
@@ -27,6 +28,7 @@ app.locals.appName = 'NodePop'
 //routes
 app.use(sessionManager.middleware)
 app.use(sessionManager.useSessionInViews)
+app.use(i18n.init)
 app.use('/', homeRoutes)
 app.use('/products', productRoutes)
 app.use('/login', loginRoute)
