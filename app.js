7f228c9fac9b72f5118a2c63e31d90ed9583eb4a
Sara <saramf240614@gmail.com>
2025-06-21 13:49:37 -0400

    Instalamos cookieparser para decirle a nuestra app que lea la cookie. La agregamos en app.js y ya está funcionando nuestro selector de idioma

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ import * as sessionManager from './lib/sessionManager.js'
 import { logoutUser } from './controllers/loginController.js'
 import i18n from './lib/i18nConfigure.js'
 import { changeLocale } from './controllers/localeController.js'
+import cookieParser from 'cookie-parser'
 
 
 await connectMongoose()
@@ -27,6 +28,7 @@ app.engine('html', ejs.__express)
 app.locals.appName = 'NodePop'
 
 //routes
+app.use(cookieParser())
 app.use(sessionManager.middleware)
 app.use(sessionManager.useSessionInViews)
 app.use(i18n.init)
